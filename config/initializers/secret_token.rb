c52d6f4db8a63338f96cbbd8c3e800b805a80d9e
Shota Fukumori (sora_h) <her@sorah.jp>
2013-08-03 12:47:42 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nikomaki::Application.config.secret_key_base = '618f21914f4256795bfc90a73ef7219f60f02bc86da2ab3badae2f856c38713e14b1272a9387fe82b1c18f5ff417f3d25aafdca568d00b85ccd72ababd1c312f'
