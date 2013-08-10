0da91810c759ec842388d758cf355f16add994a4
Shota Fukumori (sora_h) <her@sorah.jp>
2013-08-11 04:54:40 +0900

    Edit accounts/ views

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -69,6 +69,6 @@ class AccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
-      params.require(:account).permit(:name, :meta)
+      params.require(:account).permit(:name, :meta, :icon)
     end
 end
