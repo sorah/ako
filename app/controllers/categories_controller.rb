a72377fc032bb43e81a888f2de81cff1c7241aff
Shota Fukumori (sora_h) <her@sorah.jp>
2013-10-20 21:25:15 +0900

    Category: fixed checkbox

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -70,7 +70,7 @@ class CategoriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
       params.require(:category) \
-            .permit(:name, :budget, :order,
+            .permit(:name, :budget, :order, :fixed,
                     sub_categories_attributes: %i[name id order _destroy])
     end
 end
