148b6b4d7b153d104121b75a52c47e00ed1755e1
Shota Fukumori (sora_h) <her@sorah.jp>
2014-01-04 03:05:05 +0900

    Delegate year and month to FiscalDate::Month

--- app/models/report/month.rb
+++ app/models/report/month.rb
@@ -5,7 +5,6 @@ class Report
     def initialize(*args)
       case args.size
       when 2
-        @year, @month = args
         @fiscal_month = FiscalDate::Month.new(*args) # year, month
       when 1
         unless args.first.kind_of?(FiscalDate::Month)
@@ -13,16 +12,18 @@ class Report
         end
 
         @fiscal_month = args.first
-        @year, @month = @fiscal_month.year, @fiscal_month.month
       else
         raise ArgumentError, "wrong number of arguments (#{args.size} for 1..2)"
       end
     end
 
-    attr_reader :fiscal_month, :year, :month
+    attr_reader :fiscal_month
+
+    def year() @fiscal_month.year end
+    def month() @fiscal_month.month end
 
     def budget
-      Category.total_budget(@year, @month)
+      Category.total_budget(year, month)
     end
 
     def expenses
