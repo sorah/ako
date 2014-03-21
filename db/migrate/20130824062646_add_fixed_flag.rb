e66a4fa99ec55f8e444698a4c10f3060cb3180c7
Shota Fukumori (sora_h) <her@sorah.jp>
2014-03-22 02:54:07 +0900

    Add missing index declaration

--- db/migrate/20130824062646_add_fixed_flag.rb
+++ db/migrate/20130824062646_add_fixed_flag.rb
@@ -2,5 +2,7 @@ class AddFixedFlag < ActiveRecord::Migration
   def change
     add_column :payments, :fixed, :boolean
     add_column :categories, :fixed, :boolean
+
+    add_index :payments, [:paid_at, :fixed]
   end
 end
