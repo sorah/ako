c059899e1a04d1b124caf7277c571bdc91f9a8de
Shota Fukumori (sora_h) <her@sorah.jp>
2014-04-15 12:50:30 +0900

    Don't query the same query

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,7 @@ class Place < ActiveRecord::Base
 
   validates :name, presence: true
 
+  # rubocop:disable CyclomaticComplexity
   def self.candidates_for_expense(query, name_only: false)
     return [] unless query
 
@@ -13,13 +14,15 @@ class Place < ActiveRecord::Base
 
     # For Japanese (CJK Unified Ideographs = \u4e00 - 9fff)
     if candidates.empty? && /[\u4e00-\u9fff]|\p{Hiragana}|\p{Katakana}/ =~ query
-      query = query \
+      new_query = query \
         .gsub(/[a-zａ-ｚ]$/, '') # Incomplete romaji
         .gsub(/[▼▽].*$/, '') # For SKK users
 
-      candidates = search_by_name(query).select(name_only ? [:id, :name] : '*')
+      return [] if new_query == query
+      candidates = search_by_name(new_query).select(name_only ? [:id, :name] : '*')
     end
 
     candidates
   end
+  # rubocop:enable CyclomaticComplexity
 end
