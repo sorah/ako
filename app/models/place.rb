0d00a26e873f86d9ff0adf61ae531575f0f6df27
Shota Fukumori (sora_h) <her@sorah.jp>
2014-04-15 12:50:23 +0900

    Load earlier to avoid invalid statement
    This is Rails 4.0.0's bug:
    
    - https://github.com/rails/rails/commit/968c581ea34b5236af14805e6a77913b1cb36238
    - https://github.com/rails/rails/issues/13648

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,7 +9,7 @@ class Place < ActiveRecord::Base
   def self.candidates_for_expense(query, name_only: false)
     return [] unless query
 
-    candidates = search_by_name(query).select(name_only ? [:id, :name] : '*')
+    candidates = search_by_name(query).select(name_only ? [:id, :name] : '*').load
 
     # For Japanese (CJK Unified Ideographs = \u4e00 - 9fff)
     if candidates.empty? && /[\u4e00-\u9fff]|\p{Hiragana}|\p{Katakana}/ =~ query
